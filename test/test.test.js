fbc2c061e9f8b8781a1034def03ecc47e8a9e210
Väshnak <totorinouchet2a@gmail.com>
2015-10-27 20:59:18 +0100

    test de couverture de branche avec istanbul

---
+++ test/test.test.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var test = require('../modules/test.js');
+
+describe('Penis Test', function () {
+
+    it ('LoL', function (done) {
+        assert(test.test(13 === 1);
+        done();
+    });
+
+    it ('LoL 2', function (done) {
+        assert(test.test(16) === 2);
+        done();
+    });
+
+    it ('LoL 3', function (done) {
+        assert(test.test(25) === 3);
+        done();
+    });
+});
